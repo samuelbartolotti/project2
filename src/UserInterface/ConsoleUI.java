0208718eb2b9f2857a918a1cd69857544e33b105
samuelbartolotti <samuel.bartolotti@gmail.com>
2026-01-17 18:15:21 +0100

    pridani metod do ConsoleUI

--- src/UserInterface/ConsoleUI.java
+++ src/UserInterface/ConsoleUI.java
@@ -2,4 +2,18 @@ package UserInterface;
 
 public class ConsoleUI implements UI {
 
+    @Override
+    public void print(String string) {
+
+    }
+
+    @Override
+    public String scanLine() {
+        return "";
+    }
+
+    @Override
+    public void println(String string) {
+
+    }
 }
