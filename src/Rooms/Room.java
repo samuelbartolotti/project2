fae6dff903af3a77c057f28dd49cd8676e8f323c
samuelbartolotti <samuel.bartolotti@gmail.com>
2026-02-01 21:50:29 +0100

    rozsireni tridy Room

--- src/Rooms/Room.java
+++ src/Rooms/Room.java
@@ -2,10 +2,43 @@ package Rooms;
 
 import Interface.RandomGenerator;
 
+import java.awt.*;
+
 public abstract class Room extends RandomGenerator {
     protected final int maxLength = 20;
     protected final int minLength = 10;
 
     protected int width;
     protected int length;
+
+    public int getMaxLength() {
+        return maxLength;
+    }
+
+    public int getMinLength() {
+        return minLength;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public Point generateXY(){
+       int width = rnd(minLength, maxLength);
+       int length = rnd(minLength, maxLength);
+
+       return new Point(width, length);
+    }
 }
