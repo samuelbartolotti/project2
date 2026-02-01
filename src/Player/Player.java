eae5a235af35d380d53924604a447a3ab840d295
samuelbartolotti <samuel.bartolotti@gmail.com>
2026-02-01 21:03:12 +0100

    Pridani logiky tridy Player

--- src/Player/Player.java
+++ src/Player/Player.java
@@ -1,4 +1,58 @@
 package Player;
 
 public class Player {
+    private String name;
+    private int kills;
+    private int deaths;
+    private int level;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getKills() {
+        return kills;
+    }
+
+    public void setKills(int kills) {
+        this.kills = kills;
+    }
+
+    public void addKill(){
+        this.kills++;
+    }
+
+    public int getDeaths() {
+        return deaths;
+    }
+
+    public void setDeaths(int deaths) {
+        this.deaths = deaths;
+    }
+
+    public void addDeath(){
+        this.deaths++;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public void addLevel(){
+        this.level++;
+    }
+
+    public Player() {
+        kills = 0;
+        deaths = 0;
+        level = 1;
+    }
 }
