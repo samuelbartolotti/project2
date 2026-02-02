013027612ccd5f9f67e78f9f1d3a52d58460116b
samuelbartolotti <samuel.bartolotti@gmail.com>
2026-02-02 22:34:05 +0100

    vytvoreni construktoru pro consumable a potrebne metody pro vytvoreni

--- src/Items/Consumables.java
+++ src/Items/Consumables.java
@@ -1,8 +1,9 @@
 package Items;
 
+import Interface.RandomGenerator;
 import Player.Player;
 
-public class Consumables {
+public class Consumables extends RandomGenerator {
     private Effect effect;
     private Rarity rarity;
 
@@ -22,12 +23,40 @@ public class Consumables {
         this.rarity = rarity;
     }
 
-    public Consumables(Effect effect, Rarity rarity) {
-        this.effect = effect;
-        this.rarity = rarity;
+    public Effect generateEffect() {
+        int eff = rnd(0, 3);
+
+        return switch (eff) {
+            case 0 -> Effect.HEAL;
+            case 1 -> Effect.ADDMAXHEALTH;
+            case 2 -> Effect.DEFENCE;
+            case 3 -> Effect.ATTACK;
+            default -> null;
+        };
+    }
+
+    public Rarity generateRarity() {
+        int r = rnd(0, 3);
+        return switch (r) {
+            case 0 -> Rarity.COMMON;
+            case 1 -> Rarity.RARE;
+            case 2 -> Rarity.EPIC;
+            case 3 -> Rarity.LEGENDARY;
+            default -> null;
+        };
+    }
+
+    public Consumables() {
+        this.effect = generateEffect();
+        this.rarity = generateRarity();
     }
 
     public void addEffect(Player player) {
         effect.apply(player, rarity.returnEffect());
+    }
+
+    @Override
+    public String toString() {
+        return rarity.colorize("C");
     }
 }
