f716d0c36efd596e8aed918f58bca2430a23a170
bartolotti <samuel.bartolotti@gmail.com>
2026-02-10 09:06:24 +0100

    mini zmeny

--- src/PlayerCommand/Move.java
+++ src/PlayerCommand/Move.java
@@ -9,13 +9,13 @@ public class Move extends Command {
     @Override
     public void execute(String str, Player player, Room room) {
         if (str.equals("w") || str.equals("s") || str.equals("a") || str.equals("d")) {
-            return switch (str) {
-                case "w" -> if(room.isPlaceEmpty(player.getX(), player.getY(), room) == true){};
-                case "s" -> ;
-                case "a" -> ;
-                case "d" -> ;
-                default -> null;
-            };
+//            return switch (str) {
+//                case "w" -> if(room.isPlaceEmpty(player.getX(), player.getY(), room) == true){};
+//                case "s" -> ;
+//                case "a" -> ;
+//                case "d" -> ;
+//                default -> null;
+//            };
         }
     }
 
